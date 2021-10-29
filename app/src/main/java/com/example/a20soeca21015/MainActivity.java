20d0421801a08f1613303c8052dcd2fab6aea216
Admin <mjalalji995@rku.ac.in>
2021-10-29 08:41:09 +0530

    Tutorial02

---
+++ app/src/main/java/com/example/a20soeca21015/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a20soeca21015;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle(getResources().getString(R.string.app_title));
+    }
+}
